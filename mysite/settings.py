f3fede14ca345d59356958e2dab761f7fe2efd5e
chiraag-kakar <chirucos1998@gmail.com>
2020-09-23 01:58:24 +0530

    changed host settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0y!an-xjfg2(a&1p49r#pv)g-&8g2sz(859-d71)8gll3(nfwh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
