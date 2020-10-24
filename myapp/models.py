48e130825994b82876c67d939fc5f17106efd0a2
chiraag-kakar <chirucos1998@gmail.com>
2020-10-25 00:25:56 +0530

    created database models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Signup(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    contact = models.CharField(max_length=10,null=True)
+    branch = models.CharField(max_length=50,null=True)
+    role = models.CharField(max_length=20,null=True)
+
+    def __str__(self):
+        return self.user.userame
+
+class Notes(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    uploadingdate = models.CharField(max_length=30)
+    branch = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    notesfile = models.FileField(null=True)
+    filetype = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    status = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.signup.user.userame+" "+self.status
