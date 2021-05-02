cb551340997bd20ddc37758f1a13fc5a1e4ef064
Hari Ram J P <hariramjp777@gmail.com>
2021-05-02 17:11:05 +0530

    Update scripts, Add script for focus/blur inputs

--- myapp/static/js/scripts.js
+++ myapp/static/js/scripts.js
@@ -3,3 +3,12 @@ const navList = document.querySelector(".nav-list");
 navBtn.addEventListener("click", function () {
   navList.classList.toggle("view");
 });
+const inputs = document.querySelectorAll(".input-field > *");
+inputs.forEach(function (input) {
+  input.addEventListener("focus", function () {
+    this.parentElement.classList.add("focusing");
+  });
+  input.addEventListener("blur", function () {
+    this.parentElement.classList.remove("focusing");
+  });
+});
