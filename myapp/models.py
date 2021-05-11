d3ac3831f65fa66676da2305ec6a4fbcbae6cd2b
Hari Ram J P <hariramjp777@gmail.com>
2021-05-11 19:07:03 +0530

    Update models.py
    Add field for profile

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,7 @@ class Signup(models.Model):
     contact = models.CharField(max_length=10,null=True)
     branch = models.CharField(max_length=50,null=True)
     role = models.CharField(max_length=20,null=True)
+    profile_photo = models.FileField(null=True)
 
     def __str__(self):
         return self.user.username
