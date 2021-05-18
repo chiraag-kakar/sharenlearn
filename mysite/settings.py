478bf42aa8090ad2cd4597347c618ecffb5deeea
Chiraag Kakar <chirucos1998@gmail.com>
2021-05-18 15:26:33 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,8 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # SMTP backend
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'example@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'superuser.sharenlearn@gmail.com'
+EMAIL_HOST_PASSWORD = '71517151superuser'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
